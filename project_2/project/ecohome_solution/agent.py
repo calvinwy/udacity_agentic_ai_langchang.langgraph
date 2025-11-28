3d60ead1c8e624c535f63e5ee09ba654f946a490
calvinwy <28447210+calvinwy@users.noreply.github.com>
2025-11-28 23:38:00 +0100

    Reupload all files in project_2 folder.

---
+++ project_2/project/ecohome_solution/agent.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+# from langchain_core.messages import SystemMessage
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage, ToolMessage
+from langgraph.prebuilt import create_react_agent
+from tools import TOOL_KIT
+
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm_base_url = "https://openai.vocareum.com/v1"
+
+class Agent:
+
+    def __init__(self, instructions:str, model:str="gpt-4o-mini"):
+
+        # Initialize the LLM
+        llm = ChatOpenAI(
+            model=model,
+            temperature=0.0,
+            base_url="https://openai.vocareum.com/v1",
+            api_key=OPENAI_API_KEY,
+        )
+
+        # Create the Energy Advisor agent
+        self.graph = create_react_agent(
+            name="energy_advisor",
+            prompt=SystemMessage(content=instructions),
+            model=llm,
+            tools=TOOL_KIT,
+        )
+
+    def invoke(self, question: str, context:str=None) -> str:
+        """
+        Ask the Energy Advisor a question about energy optimization.
+        
+        Args:
+            question (str): The user's question about energy optimization
+            context (str): Relevant contextual information related to the question
+        
+        Returns:
+            str: The advisor's response with recommendations
+        """
+        
+        messages = []
+        if context:
+            # Add some context to the question as a system message
+            messages.append(
+                ("system", context)
+            )
+
+        messages.append(
+            ("user", question)
+        )
+        
+        # Get response from the agent
+        response = self.graph.invoke(
+            input= {
+                "messages": messages
+            }
+        )
+        
+        return response
+
+    def get_agent_tools(self):
+        """Get list of available tools for the Energy Advisor"""
+        return [t.name for t in TOOL_KIT]
