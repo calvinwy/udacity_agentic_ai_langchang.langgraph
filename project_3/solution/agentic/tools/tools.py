8c41728d73698e43cd050a31e5fd41707ed06e8e
calvinwy <28447210+calvinwy@users.noreply.github.com>
2025-12-05 00:25:36 +0100

    Creating sample data in progress.

---
+++ project_3/solution/agentic/tools/tools.py
@@ -0,0 +1,30 @@
+import os
+from typing import Dict, Any, List, Literal
+from IPython.display import Image, display
+from pydantic import BaseModel, Field
+import nest_asyncio
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables.graph import MermaidDrawMethod
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.graph import START, END, StateGraph
+from langgraph.graph.message import MessagesState
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm_base_url = "https://openai.vocareum.com/v1"
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.0,
+    base_url="https://openai.vocareum.com/v1",
+    api_key=OPENAI_API_KEY,
+)
+
+@tool
+def rag_search():
